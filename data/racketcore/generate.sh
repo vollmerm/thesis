ce636d0fcf7cb648c9e5d2f489a354478476b9e2
Michael Vollmer <vollmerm@indiana.edu>
2020-11-08 05:09:43 +0000

    add racket core map plot

--- data/racketcore/generate.sh
+++ data/racketcore/generate.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 DATA_DIR=$1
 
 gnuplot -e "data_dir='$DATA_DIR'" $DATA_DIR/slowdown_countnodes.gpi
+gnuplot -e "data_dir='$DATA_DIR'" $DATA_DIR/slowdown_treewalk.gpi
 
 cp $DATA_DIR/slowdown_countnodes_plot.tex ./
 cp $DATA_DIR/slowdown_countnodes_plot.eps ./
+cp $DATA_DIR/slowdown_treewalk_plot.tex ./
+cp $DATA_DIR/slowdown_treewalk_plot.eps ./
